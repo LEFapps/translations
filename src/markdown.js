1330df1dc45a5123ab8fa9e0a7996acb2eb5dbf0
johanlef <jb@eenwereldmetlef.be>
2020-09-29 13:56:47 +0200

    Add markdown-it-anchor

--- src/markdown.js
+++ src/markdown.js
@@ -4,6 +4,8 @@ const markdown = MarkdownIt({
   html: true,
   linkify: true,
   typography: true
-}).use(require('markdown-it-video'))
+})
+  .use(require('markdown-it-video'))
+  .use(require('markdown-it-anchor'))
 
 export default markdown
